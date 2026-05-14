994ebbc97a5bc03d8f5d4bfc27b724417cbef299
hj-66 <a01088200534@gmail.com>
2026-05-14 16:50:54 +0900

    2-04 엔티티로 테이블 매핑하기

---
+++ src/main/java/com/mysite/sbb/Question.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> createdDate;
+}
