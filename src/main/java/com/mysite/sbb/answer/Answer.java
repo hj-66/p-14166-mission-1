2c3a163c6087d8b73d8095c6b5650a66765a6413
hj-66 <a01088200534@gmail.com>
2026-05-15 17:14:10 +0900

    2-06 도메인별로 분류하기

--- src/main/java/com/mysite/sbb/answer/Answer.java
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -1,25 +1,25 @@
-package com.mysite.sbb;
+package com.mysite.sbb.answer;
 
+import java.time.LocalDateTime;
+
+import com.mysite.sbb.question.Question;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 @Entity
-public class Question {
+public class Answer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(length = 200)
-    private String subject;
-
     @Column(columnDefinition = "TEXT")
     private String content;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-    private List<Answer> createdDate;
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
 }
